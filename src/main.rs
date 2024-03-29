7c320eb528c3f22ae88406fa0853e623273049ef
Krzysztof Kuś HP WSL <kuskrz@gmail.com>
2024-03-29 23:00:13 +0100

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short = 'n', long = "noout", default_value_t = false)]
+    noout: bool,
+
+    #[arg(short, long)]
+    response: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("nout: {}", args.noout);
+    match args.response {
+        Some(v) => println!("response: {}", v),
+        None => println!("response: empty"),
+    }
 }
