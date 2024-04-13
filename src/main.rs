20fc02121642d05aa518ea019d060b561f309923
Krzysztof Kuś HP WSL <kuskrz@gmail.com>
2024-04-13 23:10:48 +0200

    TLS added

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod router;
 
 use crate::router::init_router;
 
-use std::{fs, net::SocketAddr, path::PathBuf};
+use std::{fs, net::SocketAddr, path::PathBuf, process};
 
 use axum_server::tls_rustls::RustlsConfig;
 use clap::{ArgAction, Parser};
@@ -13,9 +13,17 @@ use clap::{ArgAction, Parser};
 struct Args {
     /// port number
     #[arg(short = 'p', long = "port", default_value_t = 2024)]
-    port: u32,
+    port: u16,
 
-    /// do not produce stdout
+    /// key file
+    #[arg(short = 'k', long = "key")]
+    key: Option<String>,
+
+    /// cert file
+    #[arg(short = 'c', long = "cert")]
+    cert: Option<String>,
+
+    /// sink mode
     #[arg(
         short = 's',
         long = "sink",
@@ -50,15 +58,9 @@ struct RequestConfig {
     response_body: String,
 }
 
-#[derive(Clone, Copy)]
-struct Ports {
-    http: u16,
-    https: u16,
-}
-
 #[tokio::main]
 async fn main() {
-    print!("\u{1f980} ");
+    println!("\u{1f980} ");
     let args = Args::parse();
     let mut response = "".to_owned();
 
@@ -78,32 +80,36 @@ async fn main() {
     };
 
     let app = init_router(request_config);
-    /*  let socket = String::from("0.0.0.0:") + &args.port.to_string();
-    println!("Listening on {}", socket);
-    // unwrap - no sense to start without socket and server
-    let listener = tokio::net::TcpListener::bind(socket).await.unwrap();
-    axum::serve(listener, app).await.unwrap(); */
 
-    // https://github.com/tokio-rs/axum/blob/main/examples/tls-rustls/src/main.rs
-    let ports = Ports {
-        http: 7878,
-        https: 3000,
-    };
+    if args.key.is_none() || args.cert.is_none() {
+        let socket = String::from("0.0.0.0:") + &args.port.to_string();
+        println!("Listening on {}", socket);
+        // unwrap - no sense to start without socket and server
+        let listener = tokio::net::TcpListener::bind(socket).await.unwrap();
+        axum::serve(listener, app).await.unwrap();
+    } else {
+        // https://github.com/tokio-rs/axum/blob/main/examples/tls-rustls/src/main.rs
+        let key = args.key.expect("key not set");
+        let key_file = PathBuf::from(key);
+        if !key_file.exists() {
+            println!("key file does not exist");
+            process::exit(2);
+        }
+        let cert = args.cert.expect("cet not set");
+        let cert_file = PathBuf::from(cert);
+        if !cert_file.exists() {
+            println!("cert file does not exist");
+            process::exit(2);
+        }
 
-    let config = RustlsConfig::from_pem_file(
-        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-            .join("self_signed_certs")
-            .join("cert.pem"),
-        PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-            .join("self_signed_certs")
-            .join("key.pem"),
-    )
-    .await
-    .unwrap();
+        let config = RustlsConfig::from_pem_file(cert_file, key_file)
+            .await
+            .unwrap();
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], ports.https));
-    axum_server::bind_rustls(addr, config)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+        let addr = SocketAddr::from(([0, 0, 0, 0], args.port));
+        axum_server::bind_rustls(addr, config)
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+    }
 }
