ef2f9ff0ef1e05b4b7ceb2197378dff2b88d0e75
Krzysztof Kuś (Ubuntu) <kuskrz@gmail.com>
2024-10-10 21:08:04 +0200

    build

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
+rm install/linux-gnu/* install/linux-musl/* install/windows/*
 cargo build --release
 RUSTFLAGS="-C target-feature=+crt-static" cargo build --target x86_64-unknown-linux-gnu --release
 cargo build --release --target x86_64-unknown-linux-musl
-cargo build --release --target x86_64-pc-windows-gnu
-
+#cargo build --release --target x86_64-pc-windows-gnu
+mv target/release/websink install/linux-gnu/
+mv target/x86_64-unknown-linux-gnu/release/websink install/linux-gnu/websink_gnu_static
+mv target/x86_64-unknown-linux-musl/release/websink install/linux-musl/websink_musl_static
